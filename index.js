40973ca582dc6aa6683c0eb3dc80e82d471ad10a
sneha shinde <Sneha06032000@gmail.com>
2022-09-11 12:58:53 +0530

    cat images changing

---
+++ index.js
@@ -0,0 +1,15 @@
+const cat_result = document.getElementById( 'cats' );
+const cat_btn = document.getElementById( 'btn' );
+
+
+cat_btn.addEventListener( 'click', getRandomCat );
+
+function getRandomCat() {
+	fetch( 'https://aws.random.cat/meow' )
+		.then( res => res.json() )
+		.then( data => {
+			cat_result.innerHTML = `<img src="${data.file}" />`;
+		} );
+    }
+
+    
